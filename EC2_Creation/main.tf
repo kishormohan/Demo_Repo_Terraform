c063b69038b13d3367d4850ea30e50af2d124b2e
Kishor Kumar <31897308+kishormohan@users.noreply.github.com>
2024-05-22 22:34:16 +0530

    Update main.tf

--- EC2_Creation/main.tf
+++ EC2_Creation/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 resource "aws_instance" "ec2_instance1" {
     ami = "ami-04b70fa74e45c3917"
     instance_type = "t2.micro"
-    provider = "aws.region1"
+    provider = aws.region1
     tags = {
     Name = "Instance1"
   }
@@ -20,7 +20,7 @@ resource "aws_instance" "ec2_instance1" {
 resource "aws_instance" "ec2_instance2" {
     ami = "ami-09040d770ffe2224f"
     instance_type = "t2.micro"
-    provider = "aws.region2"
+    provider = aws.region2
     tags = {
     Name = "Instance2"
   }
